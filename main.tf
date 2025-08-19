455d5cf58cf9e41972e8d970bec398df82da9c44
aseapuree <jampierrets@gmail.com>
2025-08-19 00:07:59 -0500

    ADD: tfplan y custom config for modules

--- main.tf
+++ main.tf
@@ -14,3 +14,67 @@
 #       04.sg.tf        \ 
 #       terraform.tfvars
 
+
+
+
+#### tfstate ###
+terraform {
+  backend "s3" {
+    bucket    = "webinar-terraform-caosbinario-123aaa1234"
+    key       = "project01/terraform.tfstate" 
+    region    = "us-east-1"
+  }
+}
+
+
+## MODULOS
+## Son una forma de encapsular y reutilizar bloques de configuracion
+## Por cada modulo que se agregue se debe de ejecutar: terraform init
+## Para el capitulo de modulos no es necesario .tfvars
+module "nginx_server_dev" {
+  source = "./nginx_server_module"
+
+  ami_id        = "ami-0440d3b780d96b29d"
+  instance_type = "t3.micro"
+  server_name   = "nginx_server_dev"
+  environment   = "dev"
+}
+
+module "nginx_server_qa" {
+  source = "./nginx_server_module"
+
+  ami_id        = "ami-0440d3b780d96b29d"
+  instance_type = "t3.micro"
+  server_name   = "nginx_server_qa"
+  environment   = "qa"
+}
+
+
+##Definicion de ouputs    
+output "nginx_dev_ip" {
+  description = "Direccion IP publica de la instancia EC2"
+  value       = module.nginx_server_dev.server_public_ip
+}
+
+output "nginx_dev_dns" {
+  description = "DNS publico de la instancia EC2"
+  value       = module.nginx_server_dev.server_public_dns
+}
+
+output "nginx_qa_ip" {
+  description = "Direccion IP publica de la instancia EC2"
+  value       = module.nginx_server_qa.server_public_ip
+}
+
+output "nginx_qa_dns" {
+  description = "DNS publico de la instancia EC2"
+  value       = module.nginx_server_qa.server_public_dns
+}
+
+## GUARDAR plan
+## terraform plan -out server_qa.tfplan
+## terraform apply "server_qa.tfplan"
+
+##.tfstate
+##El archivo de estado de Terraform es un archivo que registra el estado actual de la infraestructura administrada por terraterraform {
+##No es correcto que se quede en la maquina local  
--- nginx_server_module/03.key.tf
+++ nginx_server_module/03.key.tf
@@ -1,7 +1,7 @@
 ## CLAVES
 ## ssh-keygen -t rsa -b 2048 -f "nginx-server.key"
 resource "aws_key_pair" "nginx-server-ssh" {
-   key_name       = "nginx-server-ssh"
+   key_name       = "nginx-server-ssh-${var.environment}"
    public_key     = file("nginx-server.key.pub")
 
    tags = {
